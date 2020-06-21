60684a0a3d9d616cda4529850002bdf8755d237c
smg19 <suhanighorpade18@gmail.com>
2020-06-21 21:22:57 +0530

    cloned

---
+++ Components/MainChat/Sub-Components/messageList.js
@@ -0,0 +1,170 @@
+import React from "react";
+import { Scrollbars } from "react-custom-scrollbars";
+import Prism from 'prismjs';
+import Dropdown from 'react-bootstrap/Dropdown'
+import { ButtonGroup } from 'react-bootstrap';
+import GetAppRoundedIcon from '@material-ui/icons/GetAppRounded';
+import { connect } from 'react-redux'
+import { downloadFile } from '../../../store/actions/chatActions';
+import "./messageList.css"
+var fileDownload = require('js-file-download');
+
+
+function TextMessage(props) {
+    const [hover, setHover] = React.useState(false);
+
+    function toggleHover() {
+        console.log("HOVERED")
+        setHover(!hover)
+    }
+
+    const getCoords = (url) => {
+        console.log(url);
+        const coords = url.split("=")[1].split(",");
+        console.log([coords[1], coords[0]]);
+        return coords[1].toString() + ", " + coords[0].toString();
+    };
+
+    if (props.messageObject.type === "location") {
+        getCoords(props.messageObject.content);
+    }
+
+    const DropdownElement = () => (
+        <Dropdown as={ButtonGroup}>
+            <Dropdown.Toggle split variant="success" id="dropdown-split-basic" size="sm" />
+
+            <Dropdown.Menu id="drop">
+                <Dropdown.Item id="menuitem" onClick={() => { console.log("Report!"); console.log(props.messageObject.id); props.reportOnClick(props.messageObject.id) }}>Report</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    )
+
+    const fileDownloaded = (file, fileName) => {
+        // console.log("HEY");
+        // console.log(file);
+        fileDownload(file, fileName);
+    }
+
+    return (
+        <div className="message" key={props.keyValue} >
+            {props.messageObject.owner === props.currentUser ? (
+                <div className="spacer"></div>
+            ) : null}
+            <div
+                onMouseEnter={toggleHover}
+                onMouseLeave={toggleHover}
+
+                className={
+                    "message-area" +
+                    (props.messageObject.owner === props.currentUser
+                        ? " current-user"
+                        : " other-user") + (props.messageObject.type.includes('code') ? " code-area" : "") + (props.messageObject.type.includes('file') ? " file-area" : "")
+                }
+            >
+                <div className="message-user" >
+                    <div className="user-name">{props.messageObject.owner}</div>
+                    <div className="spacer"></div>
+                    {hover && !props.messageObject.isReported && props.conversationType === "channels" ? <div className="options" ><DropdownElement /></div> : null}
+                </div>
+
+                {props.messageObject.type === "text" ? (
+                    <div className="message-content">{props.messageObject.content}</div>
+                ) : props.messageObject.type === "location" ? (
+                    <div className="location-content">
+                        <a href={props.messageObject.content}>
+                            {props.messageObject.content}
+                        </a>
+                        <img
+                            src={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/${getCoords(
+                                props.messageObject.content
+                            )},11,0/400x200@2x?access_token=pk.eyJ1Ijoic2F0dmlrZGFuZGFsZSIsImEiOiJja2JpMmJkdGQwYjZhMnRwamlmYmhmZDQ5In0.xEAG7PvsDEt0lM4PCUQ_NA&logo=false`}
+                            alt="location"
+                            className="static-map"
+                        ></img>
+                    </div>
+                ) : props.messageObject.type.includes('code') ?
+                            (
+                                <div className="message-content "><pre><code className={"language-" + props.messageObject.type.split("/")[1].toLowerCase()}>{`${props.messageObject.content}`}</code></pre></div>
+                            ) : (   // It is file
+                                <div
+                                    className="message-content"
+                                    onClick={() => {
+                                        props.downloadFile(props.messageObject.content, (file) => {
+                                            fileDownloaded(file, props.messageObject.type.split("/")[1])
+                                        })
+                                    }}
+                                ><GetAppRoundedIcon />{props.messageObject.type.split("/")[1]}</div>
+                            )
+
+                }
+                <div className="message-time">{props.messageObject.time}</div>
+            </div>
+        </div>
+    );
+}
+
+class MessageList extends React.Component {
+    reportOnClick = (Mid) => {
+        console.log("REPORT CLICKED")
+        console.log(Mid)
+        this.props.reportMessageFromChat(Mid);
+    }
+
+    componentDidMount() {
+        Prism.highlightAll();
+        const height = this.refs.messageScrollbar.getScrollHeight();
+        this.refs.messageScrollbar.scrollTop(height);
+    }
+
+    componentDidUpdate() {
+        Prism.highlightAll();
+        const height = this.refs.messageScrollbar.getScrollHeight();
+        this.refs.messageScrollbar.scrollTop(height);
+    }
+
+    render() {
+        console.log(this.props);
+        return (
+            <Scrollbars autoHide ref="messageScrollbar">
+                {this.props.messageList.map((message, index) => {
+                    let date = new Date(message.createdAt);
+                    const time = date.toTimeString().split(" ")[0];
+                    console.log(message)
+                    let messageObject = {
+                        content: message.isReported ? "This message was reported" : message.content,
+                        owner: "unknown",
+                        type: message.isReported ? "text" : message.type,
+                        time: time,
+                        id: message._id,
+                        isReported: message.isReported
+                    };
+                    console.log(messageObject)
+                    if (message.owner) {
+                        if (message.owner.username)
+                            messageObject.owner = message.owner.username;
+                        else messageObject.owner = message.owner;
+                    }
+                    Prism.highlightAll();
+                    return (
+                        <TextMessage
+                            conversationType={this.props.conversationType}
+                            keyValue={index}
+                            messageObject={messageObject}
+                            currentUser={this.props.currentUser}
+                            downloadFile={this.props.downloadFile}
+                            reportOnClick={this.reportOnClick}
+                        />
+                    );
+                })}
+            </Scrollbars>
+        );
+    }
+}
+
+const MapDispatchToProps = (dispatch) => {
+    return {
+        downloadFile: (fileAddress, callback) => { dispatch(downloadFile(fileAddress, callback)) }
+    }
+}
+
+export default connect(null, MapDispatchToProps)(MessageList);
